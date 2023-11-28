9c87eca88f00e5c084b2b371185dc69148d172bd
weslex555 <wgcoburn@outlook.com>
2023-11-27 20:53:29 -0800

    adjusted header for mobile

--- client/src/features/Header/index.tsx
+++ client/src/features/Header/index.tsx
@@ -4,13 +4,13 @@ import styles from './Header.module.css';
 export default function Header() {
   setTimeout(() => {
     const header = document.getElementsByTagName('header');
-    header[0].style.height = '30vh';
+    header[0].style.minHeight = '20vh';
   }, 3500);
 
   return (
     <header role="banner" className={styles.Header}>
       <Fade delay={500} cascade damping={1} triggerOnce>
-        <h1 className={styles.Name}>Wesley Coburn</h1>
+        <h1>Wesley Coburn</h1>
         <h2>is a front-end web developer</h2>
         <h3>and this is his portfolio</h3>
       </Fade>
