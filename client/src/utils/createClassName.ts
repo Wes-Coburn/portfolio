86e07af3155d80f23df93c3e351ac09d01a89e5a
weslex555 <wgcoburn@outlook.com>
2024-01-06 11:55:53 -0800

    fixed eslint errors

--- client/src/utils/createClassName.ts
+++ client/src/utils/createClassName.ts
@@ -1,4 +1,6 @@
-export default function CreateClassName(classNames: string | Array<string> | TemplateStringsArray) {
+export default function CreateClassName(
+  classNames: string | Array<string> | TemplateStringsArray,
+) {
   if (classNames instanceof Array) return classNames.join(' ');
-  else return classNames;
+  return classNames;
 }
